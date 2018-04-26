8d9e3e3f3ab85e6e87891abdf95a0510878a14c1
gujientsy <gujientsy@163.com>
2018-04-26 16:35:24 +0800

    appdss

--- b.py
+++ b.py
@@ -56,8 +56,86 @@ class A(object):
         A().setName()
         print('staticmethod')   #此处如果 A.setName()就会报错
 
-A.jobs()
-A.setAge()
+# A.jobs()
+# A.setAge()
+
+'''
+线程
+'''
+
+import threading
+import time
+ 
+def worker(num):
+    """
+    thread worker function
+    :return:
+    """
+    # time.sleep(3)
+    #print("Thread %d" % num)
+    return
+ 
+for i in range(200):
+    t = threading.Thread(target=worker,args=(i,),name="t.%d" % i)
+    t.start()
+    #print(t.getName())
+
+
+
+'''
+异步IO,asyncio执行的任务成为协程
+'''
+
+#非常正统的方式，运行的效果如下
+import time
+import requests
+from concurrent.futures import ThreadPoolExecutor
+ 
+NUMBERS = range(12)
+# URL = 'http://httpbin.org/get?a={}'
+ 
+# def fetch(a):
+#     r = requests.get(URL.format(a))
+#     return r.json()['args']['a']
+     
+# start = time.time()
+# with ThreadPoolExecutor(max_workers=3) as executor:
+#     for num, result in zip(NUMBERS, executor.map(fetch, NUMBERS)):
+#         print('fetch({}) = {}'.format(num, result))
+ 
+# print('Use requests+ThreadPoolExecutor cost: {}'.format(time.time() - start))
+
+
+
+#加入协程
+import asyncio
+ 
+async def run_scraper_tasks(executor):
+    loop = asyncio.get_event_loop()
+ 
+    blocking_tasks = []
+    for num in NUMBERS:
+        task = loop.run_in_executor(executor, fetch, num)
+        task.__num = num
+        blocking_tasks.append(task)
+ 
+    completed, pending = await ,asyncio.wait(blocking_tasks)
+    results = {t.__num: t.result() for t in completed}
+    for num, result in sorted(results.items(), key=lambda x: x[0]):
+        print('fetch({}) = {}'.format(num, result))
+ 
+start = time.time()
+executor = ThreadPoolExecutor(3)
+event_loop = asyncio.get_event_loop()
+ 
+event_loop.run_until_complete(
+run_scraper_tasks(executor)
+)
+ 
+print('Use asyncio+requests+ThreadPoolExecutor cost: {}'.format(time.time() - start))
+
+
+
 
 
 
