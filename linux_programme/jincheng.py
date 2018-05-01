92511fc39130eda1e778a9c38257748989f54054
gujie1216933842 <gujientsy@163.com>
2018-05-01 08:31:56 +0800

    note

--- linux_programme/jincheng.py
+++ linux_programme/jincheng.py
@@ -3,9 +3,11 @@
 # @Author: anchen
 # @Date:   2018-04-30 21:19:36
 # @Last Modified by:   anchen
-# @Last Modified time: 2018-04-30 21:22:30
+# @Last Modified time: 2018-05-01 08:31:41
 # 
 
 import os
-ret = os.fork()
+ret = os.fork()  #当执行到这段代码的时候创建新的进程
 print('haha')
+
+
