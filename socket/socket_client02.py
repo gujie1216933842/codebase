15ebb843e30007e7155133693f4de03ba708bf2c
gujie1216933842 <gujientsy@163.com>
2018-05-05 19:10:59 +0800

    note

--- socket/socket_client02.py
+++ socket/socket_client02.py
@@ -2,13 +2,13 @@
 import socket
 import json
 client = socket.socket()
-client.connect(('127.0.0.1',8888))
+client.connect(('47.97.165.75',9000))
 
-client.send(b'我爱你'.encode('utf8'))
+client.send('我爱你'.encode('utf8'))
 
 #接收数据
 data = client.recv(1024)
-print('客户端接收数据%s'%(data))
+print('客户端接收数据:%s'%(data.decode()))
 
 client.close()
 
