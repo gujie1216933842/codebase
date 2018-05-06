f661cda01a93a048758b8bff0eb4549c33829ae4
gujie1216933842 <gujientsy@163.com>
2018-05-06 15:56:42 +0800

    note

--- socket/ssh/socket_ssh_client01.py
+++ socket/ssh/socket_ssh_client01.py
@@ -6,7 +6,6 @@
 2.客户端先收大小
 3.客户端再循环接收信息
 
-
 '''
 
 import socket
@@ -19,13 +18,21 @@ import socket
 4.接收服务端返回的信息
 '''
 client = socket.socket()
-client.connect(('localhost', 9000))
+client.connect(('47.97.165.75', 9000))
 while True:
     cmd = input(">>:").strip()
     if len(cmd) == 0: continue
-    client.send(cmd.encode())
-    cmd_res = client.recv(1024)
-    print('客户端接收服务端返回的信息:%s' % (cmd_res.decode()))
-
+    client.send(cmd.encode("utf-8"))
+    cmd_size = client.recv(1024)
+    print("服务端返回的信息大小:%s"%(cmd_size))
+    content = b''
+    size = 0
+    while size < int(cmd_size.decode()) :
+        data = client.recv(1024)
+        size += len(data)
+        content +=data
+    else:
+        print(content.decode())
+    print("客户端打印的的信息大小:%s" % (size))
 
 client.close()
--- socket/ssh/socket_ssh_server01.py
+++ socket/ssh/socket_ssh_server01.py
@@ -1,7 +1,8 @@
 '''
 用socket写一个ssh客户端
 '''
-import socket,os
+import socket, os
+
 '''
 1.新建socket对象
 2.绑定
@@ -12,23 +13,24 @@ import socket,os
 '''
 
 server = socket.socket()
-server.bind(('localhost',9000))
+server.bind(('0.0.0.0', 9000))
 server.listen()
-conn,addr = server.accept()
+conn, addr = server.accept()
 while True:
     data = conn.recv(1024)
 
-    print('客户端发来的信息:%s'%(data))
+    print('客户端发来的信息:%s' % (data))
 
     cmd_res = os.popen(data.decode()).read()
     if len(cmd_res) == 0:
         cmd_res = "ssh no output....."
-    print('服务端返回给客户端的信息:%s'%(cmd_res))
+    print('服务端返回给客户端的信息:%s' % (cmd_res))
 
-    conn.send(cmd_res.encode())
+    # 传递信息的长度
+    cmd_res_size = len(cmd_res.encode("utf-8"))
+    print("系统返回的数据长度:%s"%(cmd_res_size))
+    #cmd_res = dict(content=cmd_res, content_size=cmd_res_size)
+    conn.send(str(cmd_res_size).encode("utf-8"))
+    conn.send(cmd_res.encode("utf-8"))
 
 server.close()
-
-
-
-
