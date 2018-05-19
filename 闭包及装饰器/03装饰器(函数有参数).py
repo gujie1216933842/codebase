4e348a96637dbec0825cbf43eded60ad526f3024
gujie1216933842 <gujientsy@163.com>
2018-05-19 16:52:58 +0800

    note

--- 闭包及装饰器/03装饰器(函数有参数).py
+++ 闭包及装饰器/03装饰器(函数有参数).py
@@ -1,4 +1,16 @@
-#Author:Bob
-def func():
-    func_inner():
-        
+# Author:Bob
+'''装饰器装饰有参数的函数'''
+
+def func(fun_name):
+    def func_inner(*args, **kwargs):
+        print("hahaha")
+        fun_name(*args, **kwargs)
+
+    return func_inner
+
+@func
+def index(a):
+    print("展示首页")
+
+index(1)
+
