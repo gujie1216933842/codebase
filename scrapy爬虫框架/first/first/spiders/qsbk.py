5e3880743dfd57b1429593cd951fdb14ab163138
gujie1216933842 <gujientsy@163.com>
2018-05-21 00:45:20 +0800

    note

--- scrapy爬虫框架/first/first/spiders/qsbk.py
+++ scrapy爬虫框架/first/first/spiders/qsbk.py
@@ -10,8 +10,11 @@ class QsbkSpider(scrapy.Spider):
     start_urls = (
         'http://www.qiushibaike.com/',
     )
+
     def start_requests(self):
-        
+        ua = {"User-Agent",
+              "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+        yield Request('http://www.qiushibaike.com/', headers=ua)
 
     def parse(self, response):
         # 先实例化
