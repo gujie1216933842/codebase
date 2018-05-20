4c86e43a70fe386d980329dfa16f7694e58bbaf9
gujie1216933842 <gujientsy@163.com>
2018-05-20 11:45:45 +0800

    note

--- urllib库爬虫实战/02_urllib(写入txt文件).py
+++ urllib库爬虫实战/02_urllib(写入txt文件).py
@@ -12,7 +12,7 @@ file_path = os.path.dirname(__file__)
 # print(__file__)  # 绝对路径待自己的文件名
 # print(file_path)  # 绝对路径不带自己的文件名
 # print(os.path.join(file_path, 'file')) #新文件路径加上file一级子目录
-fn = open(os.path.join(os.path.dirname(__file__),'file1.txt'),'w')
+fn = open(os.path.join(os.path.dirname(__file__),'file/file1.txt'),'w')
 #循环
 for i in mydata:
     fn.write(i+"\n")
