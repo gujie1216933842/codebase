e5d82f048d7d1364055b352517726c8f24192cf8
gujie1216933842 <gujientsy@163.com>
2018-05-26 16:24:54 +0800

    note

--- re/02_正则.py
+++ re/02_正则.py
@@ -8,12 +8,22 @@ search()方法和match()方法的区别
 import re
 
 ''' 匹配处0-100之间的整数'''
-parttern = r'\d'
-ret = re.match(parttern, "9a8")
+parttern = r'[1-9]\d?$|0$|100$' #把0和100单独考虑
+parttern1 = r'[1-9]?\d?$|100$'  #优化,把0兼容
+parttern2 = r'[1-9]?\d?0?$'     #优化1,把100兼容
+
+'''
+分析:\d后面加问号的原因是要兼容个人整数
+|  竖线是或者的意思
+$  符号是结束边界
+'''
+
+
+ret = re.match(parttern2, "99")
 print(ret)
 
-if ret:
-    print(True)
-
-ret2 = re.search(parttern,'a6j9')
-print(ret2)
+# if ret:
+#     print(True)
+#
+# ret2 = re.search(parttern,'a6j9')
+# print(ret2)
