c872b7d2dfa8fbb41dddacda6da189c18572abd4
gujie1216933842 <gujientsy@163.com>
2018-05-27 17:40:18 +0800

    note

--- python数据分析/05_折现图散点图.py
+++ python数据分析/05_折现图散点图.py
@@ -1,24 +1,26 @@
 import numpy as np
-
+import os
 import pandas as pda
 import matplotlib
+
 '''
 解决可视化的问题
+绘制折线图
 '''
 
+file_path = os.path.join(os.path.dirname(__file__), 'dd.csv')
+fn = open(file_path)
+ret = pda.read_csv(fn)
 
-ret = pda.read_csv("C:/Users/Administrator/Desktop/qq/dd.csv",encoding = 'gbk')
+'''
+注:因为这里的文件路径存在中文,所以先打卡文件在读取,实际开发中的文件路径不能存在中文,所以可直接读取
+ret = pda.read_csv(f,encoding = 'gb18030')   #gbk也可
+'''
+#
+
 
 print(ret.describe())
 
-#按照某一列排序
+# 按照某一列排序
 ret = ret.sort_values(by='price')
 print(ret)
-
-
-
-
-
-
-
-
