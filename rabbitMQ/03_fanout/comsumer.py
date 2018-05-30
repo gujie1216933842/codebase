e1ab6abb2fca7a5a4344022420d85387692566ae
gujientsy <gujientsy@163.com>
2018-05-30 14:08:44 +0800

    'aa'

--- rabbitMQ/03_fanout/comsumer.py
+++ rabbitMQ/03_fanout/comsumer.py
@@ -1,24 +1,23 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs',
-                         type='fanout')
+channel.exchange_declare(exchange='logs', type='fanout')
 # 不指定queue名字,rabbit会随机分配一个名字,exclusive=True会在使用此queue的消费者断开后,自动将queue删除
 result = channel.queue_declare(exclusive=True)
 # 获取随机的queue名字
 queue_name = result.method.queue
 print("random queuename:", queue_name)
 
-channel.queue_bind(exchange='logs',  # queue绑定到转发器上
-                   queue=queue_name)
+channel.queue_bind(exchange='logs', queue=queue_name)  # queue绑定到转发器上
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
+
 def callback(ch, method, properties, body):
     print(" [x] %r" % body)
+
 
 channel.basic_consume(callback,
                       queue=queue_name,
