fcfde8cc71ad12840e612681a07c57ee7b0f51dc
gujie1216933842 <gujientsy@163.com>
2018-06-02 15:28:47 +0800

    'aa'

---
+++ 面试编程题/02_求0到100内的质数.py
@@ -0,0 +1,16 @@
+'''
+质数:除了1和它本身以外不再有其他因数
+
+'''
+
+
+num=[];
+i=2
+for i in range(2,100):
+   j=2
+   for j in range(2,i):
+      if(i%j==0):
+         break
+   else:
+      num.append(i)
+print(num)
