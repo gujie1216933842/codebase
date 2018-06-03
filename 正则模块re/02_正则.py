1bd764dbdbba4d24e5129dc90a13472f94c793ab
gujie1216933842 <gujientsy@163.com>
2018-06-03 20:51:02 +0800

    'aa'

--- 正则模块re/02_正则.py
+++ 正则模块re/02_正则.py
@@ -8,16 +8,15 @@ search()方法和match()方法的区别
 import re
 
 ''' 匹配处0-100之间的整数'''
-pattern = r'[1-9]\d?$|0$|100$' #把0和100单独考虑
-pattern1 = r'[1-9]?\d?$|100$'  #优化,把0兼容
-pattern2 = r'[1-9]?\d?0?$'     #优化1,把100兼容
+pattern = r'[1-9]\d?$|0$|100$'  # 把0和100单独考虑
+pattern1 = r'[1-9]?\d?$|100$'  # 优化,把0兼容
+pattern2 = r'[1-9]?\d?0?$'  # 优化1,把100兼容
 
 '''
 分析:\d后面加问号的原因是要兼容个人整数
 |  竖线是或者的意思
 $  符号是结束边界
 '''
-
 
 ret = re.match(pattern2, "99")
 print(ret)
@@ -31,17 +30,20 @@ print(ret)
 
 '''匹配正整数'''
 pattern3 = r'[1-9]\d*$'
-ret3 = re.match(pattern3,'10000000')
+ret3 = re.match(pattern3, '10000000')
 print(ret3)
-
 
 '''匹配大于等于0整数'''
 pattern4 = r'[1-9]\d*$|0$'
-ret4 = re.match(pattern4,'0')
+ret4 = re.match(pattern4, '0')
 print(ret4)
 
 
 
-
+'''匹配手机号码'''
+pattern5 = r'1[3-8]\d{9}$'
+mobile = "1"
+ret5 = re.match(pattern5, '13585591803')
+print('pattern:%s' % ret5)
 
 
