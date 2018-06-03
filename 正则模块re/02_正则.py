d6d886526d96a0588e982c3cc00cec1beea352b8
gujie1216933842 <gujientsy@163.com>
2018-06-03 22:01:09 +0800

    'aa'

--- 正则模块re/02_正则.py
+++ 正则模块re/02_正则.py
@@ -38,12 +38,23 @@ pattern4 = r'[1-9]\d*$|0$'
 ret4 = re.match(pattern4, '0')
 print(ret4)
 
-
-
 '''匹配手机号码'''
-pattern5 = r'1[3-8]\d{9}$'
-mobile = "1"
+pattern5 = r'1[3,4,5,7,8]\d{9}$'     #/^1[3,4,5,7,8][0-9]{9}$
 ret5 = re.match(pattern5, '13585591803')
-print('pattern:%s' % ret5)
+print('pattern5:%s' % ret5)
+
+'''匹配空白(空格和tab键)'''
+pattern6 = r'\s'
+ret6 = re.match(pattern6, ' ')
+print('pattern6:%s' % ret6)
 
 
+'''匹配邮箱
+@之前必须有内容且只能是字母（大小写）、数字、下划线(_)、减号（-）、点（.）
+@和最后一个点（.）之间必须有内容且只能是字母（大小写）、数字、点（.）、减号（-），且两个点不能挨着
+最后一个点（.）之后必须有内容且内容只能是字母（大小写）、数字且长度为大于等于2个字节，小于等于6个字节
+
+'''
+pattern7 = r'^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$'
+ret7 = re.match(pattern7, 'gujientsy@163.com')
+print('pattern7:%s' % ret7)
