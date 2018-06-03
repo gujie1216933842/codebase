1f62a2eb67e9d6755775fd493e68d40f6cbd280a
gujie1216933842 <gujientsy@163.com>
2018-06-03 16:17:51 +0800

    'aa'

--- python文件操作/03_w_log.py
+++ python文件操作/03_w_log.py
@@ -1,30 +1,30 @@
 # coding=utf-8
 
 import os
-
 import time
+def my_log(content):
 
-# 判断是否存在脚本的兄弟层级中是否存在log文件
-log_path = os.path.join(os.path.dirname(__file__), 'log/')
-try:
-    if not os.path.exists(log_path):
-        os.makedirs(log_path)  #创建文件夹
-except Exception as e:
-    print('创建文件异常:%s' % e)
+    # 判断是否存在脚本的兄弟层级中是否存在log文件
+    log_path = os.path.join(os.path.dirname(__file__), 'log/')
+    try:
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)  # 创建文件夹
+    except Exception as e:
+        print('创建文件异常:%s' % e)
 
-# 判断当天日志文件是否存在
+    # 判断当天日志文件是否存在
 
-now_day = time.strftime('%Y-%m-%d', time.localtime())
-log_file_path = os.path.join(os.path.dirname(__file__), 'log/' + str(now_day) + '.log')
-print(log_file_path)
+    now_day = time.strftime('%Y-%m-%d', time.localtime())
+    log_file_path = os.path.join(os.path.dirname(__file__), 'log/' + str(now_day) + '.log')
 
-log_file_handle = open(log_file_path,'a')
+    log_file_handle = open(log_file_path, 'a')
 
-log_file_handle.write('你好!')
-log_file_handle.write('\n')
+    log_file_handle.write(content)
+    log_file_handle.write('\n')
 
-log_file_handle.close()
-
-
+    log_file_handle.close()
 
 
+
+if __name__ == "__main__":
+    my_log('我爱你...........')
