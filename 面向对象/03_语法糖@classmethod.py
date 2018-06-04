a776a117cfd4ddbcff48c039831efecf7cf59e95
gujie1216933842 <gujientsy@163.com>
2018-06-04 21:22:14 +0800

    aa

--- 面向对象/03_语法糖@classmethod.py
+++ 面向对象/03_语法糖@classmethod.py
@@ -2,10 +2,19 @@
 Python内置的@property装饰器就是负责把一个方法变成属性调用的：
 
 总结:普通类方法,@classmethod类方法,@staticmethod类方法区别比较
-首先方法书写形式(这里没考虑有参数,如果有参数,一次往后加)
+1.首先方法书写形式(这里没考虑有参数,如果有参数,一次往后加)
 普通:            fun(self):
 @classmethod:    fun(cls)
-@staticmethod:   fun()
+@staticmethod:   fun()  注意:这里没有self属性
+
+
+2.调用
+普通类方法的这里不再说明
+@classmethod类方法   可以实例化调用,也可以直接类名调用
+                    注意:如果是实例化调用,涉及到实例化参数的变化,对classmethod的方法结果是没有影响的,
+                    因为cls代表的类,而不是实例,cls只有类属性,即不是__init__()方法中的属性
+
+@staticmethod类方法  可以实例化调用,也可以直接类名调用,和classmethod类似
 
 
 
