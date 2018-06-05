97aa3fb049f3c766f3ffb07573e719ea414cd0e6
gujie1216933842 <gujientsy@163.com>
2018-06-05 08:24:58 +0800

    'code'

--- python-pymysql模块/01_更新.py
+++ python-pymysql模块/01_更新.py
@@ -15,17 +15,12 @@ cursor = connect.cursor()  # 最终返回数据类型元组
 # cursor = connect.cursor(cursor=pymysql.cursors.DictCursor)  #最终返回数据类型字典
 
 # 查询数据
-sql = "SELECT * from my_test WHERE id = %s limit 1 "
+sql = "update my_test set name  = %s "
 # data = (1)
-cursor.execute(sql, 1)  # 如果没有参数就不传,大于等于两个需要写成tuple形式
+cursor.execute(sql, ('乔布斯'))  # 如果没有参数就不传,大于等于两个需要写成tuple形式
 
 print('行数:%s' % cursor.rowcount)
-ret1 = cursor.fetchall()
-#ret2 = cursor.fetchone()  #cursor对象有fetch之后的第一行
-
-# for row in ret1:
-#     print(row)
-# print('共查找出', cursor.rowcount, '条数据')
+affect = cursor.rowcount
 
 cursor.close()
 connect.close()
