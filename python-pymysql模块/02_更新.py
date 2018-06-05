cb7bf2d86a4c360cdbc252de1233d999bc7a9826
gujientsy <gujientsy@163.com>
2018-06-05 12:48:27 +0800

    'code'

--- python-pymysql模块/02_更新.py
+++ python-pymysql模块/02_更新.py
@@ -11,19 +11,19 @@ connect = pymysql.Connect(
 )
 
 # 获取游标
-cursor = connect.cursor()  # 最终返回数据类型元组
+my_cursor = connect.cursor()  # 最终返回数据类型元组
 # cursor = connect.cursor(cursor=pymysql.cursors.DictCursor)  #最终返回数据类型字典
 
 # 查询数据
 sql = "update my_test set name  = %s "
 # data = (1)
-cursor.execute(sql, ('乔布斯'))  # 如果没有参数就不传,大于等于两个需要写成tuple形式
+my_cursor.execute(sql, ('乔布斯'))  # 如果没有参数就不传,大于等于两个需要写成tuple形式
 
-print('行数:%s' % cursor.rowcount)
-affect = cursor.rowcount
+print('行数:%s' % my_cursor.rowcount)
+affect = my_cursor.rowcount
 
-cursor.commit()
+my_cursor.commit()
 
-cursor.close()
+my_cursor.close()
 connect.close()
 
