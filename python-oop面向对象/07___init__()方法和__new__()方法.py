ad7b95cfa00274aaa9da39160c7c952d78dfa3ec
gujientsy <gujientsy@163.com>
2018-06-11 17:19:32 +0800

    'aa'

--- python-oop面向对象/07___init__()方法和__new__()方法.py
+++ python-oop面向对象/07___init__()方法和__new__()方法.py
@@ -4,8 +4,16 @@
 然后才会调用__init__方法
 https://www.cnblogs.com/nyist-xsk/p/8286941.html
 
-
-
-
-
 '''
+
+
+class A(object):
+    def __new__(cls, *args, **kwargs):
+        return 12
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def get(self):
+        print('hahaha')
