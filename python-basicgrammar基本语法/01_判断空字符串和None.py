2ada38766ab27f7e8df9cd3bd5a763b0ccc0e61b
gujientsy <gujientsy@163.com>
2018-06-14 20:26:45 +0800

    'aa'

---
+++ python-basicgrammar基本语法/01_判断空字符串和None.py
@@ -0,0 +1,30 @@
+'''
+在Python中，
+    None、空列表[]、空字典{}、空元组()、0等一系列代表空和无的对象会被转换成False。
+    除此之外的其它对象都会被转化成True。
+
+'''
+
+a1 = ''
+if not a1:
+    print("a1")
+
+a2 = ()
+if not a2:
+    print("a2")
+
+a3 = []
+if not a3:
+    print("a3")
+
+a4 = {}
+if not a4:
+    print("a4")
+
+a5 = None
+if not a5:
+    print("a5")
+
+a6 = None
+if not a6:
+    print("a6")
