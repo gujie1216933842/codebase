1706d1db2e6ed4edb5a9ffa39cfbb6fee52753b1
gujie1216933842 <gujientsy@163.com>
2018-06-20 07:49:23 +0800

    'aa'

---
+++ python-basicgrammar基本语法/02_json_dumps和json_loads.py
@@ -0,0 +1,9 @@
+'''
+在Python中，
+    json_dumps()    将dict转换成str
+    json_loads()    将str转换成dict
+
+'''
+
+
+
--- python-异步编程/01.py
+++ python-异步编程/01.py
@@ -1,7 +1,10 @@
 '''
 http://python.jobbole.com/87310/
 
+https://www.cnblogs.com/zjchao/p/8903552.html
 
+https://www.jianshu.com/p/45b757b40106?from=timeline
 
+http://blog.sina.com.cn/s/blog_927b416b0102xj93.html
 
 '''
