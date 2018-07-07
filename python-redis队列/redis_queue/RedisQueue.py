4f65c58ccd3b876b21799e60936080b8d0b22a04
gujie1216933842 <gujientsy@163.com>
2018-07-07 14:25:40 +0800

    aa

--- python-redis队列/redis_queue/RedisQueue.py
+++ python-redis队列/redis_queue/RedisQueue.py
@@ -21,10 +21,6 @@ class RedisQueue(object):
 
         else:
             item = self.__db.lpop(self.key)
-
-        if item:
-            item = item[1]
-
         return item
 
     def get_nowait(self):
