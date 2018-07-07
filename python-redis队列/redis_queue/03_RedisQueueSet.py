c3e5f8ba24586b5dd25711ac3767109b05bc5387
gujie1216933842 <gujientsy@163.com>
2018-07-07 14:22:38 +0800

    aa

--- python-redis队列/redis_queue/03_RedisQueueSet.py
+++ python-redis队列/redis_queue/03_RedisQueueSet.py
@@ -1,4 +1,9 @@
 import RedisQueue
 
+import time
+
 q = RedisQueue.RedisQueue('test', 'queue', host="localhost", port=6379, db=1)
-q.put('hello world')
+for i in range(5):
+    q.put(i)
+    print("input.py: data {} enqueue {}".format(i, time.strftime("%c")))
+    time.sleep(1)
--- python-redis队列/redis_queue/05_RedisCilentGet.py
+++ python-redis队列/redis_queue/05_RedisCilentGet.py
@@ -1,10 +1,10 @@
 import RedisQueue
+import time
 
 q = RedisQueue.RedisQueue('test', 'queue', host="localhost", port=6379, db=1)
-q.get()
-
-
-
-
-
-
+while 1:
+    result = q.get_nowait()
+    if not result:
+        break
+    print("output.py: data {} out of queue {}".format(result, time.strftime("%c")))
+    time.sleep(2)
