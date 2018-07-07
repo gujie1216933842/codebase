bf674697263004df5741cf938fd25b414252c710
gujie1216933842 <gujientsy@163.com>
2018-07-07 16:11:04 +0800

    aa

---
+++ python-captcha识别图形验证码/init_operate.py
@@ -0,0 +1,28 @@
+# 自适应阀值二值化
+def _get_dynamic_binary_image(filedir, img_name):
+  filename =   './out_img/' + img_name.split('.')[0] + '-binary.jpg'
+  img_name = filedir + '/' + img_name
+  print('.....' + img_name)
+  im = cv2.imread(img_name)
+  im = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY) #灰值化
+  # 二值化
+  th1 = cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 1)
+  cv2.imwrite(filename,th1)
+  return th1
+
+
+
+
+# 去除边框
+def clear_border(img,img_name):
+  filename = './out_img/' + img_name.split('.')[0] + '-clearBorder.jpg'
+  h, w = img.shape[:2]
+  for y in range(0, w):
+    for x in range(0, h):
+      if y < 2 or y > w - 2:
+        img[x, y] = 255
+      if x < 2 or x > h -2:
+        img[x, y] = 255
+
+  cv2.imwrite(filename,img)
+  return img
---
+++ python-captcha识别图形验证码/qiege.py
@@ -0,0 +1,52 @@
+def cutting_img(im, im_position, img, xoffset=1, yoffset=1):
+    filename = './out_img/' + img.split('.')[0]
+    # 识别出的字符个数
+    im_number = len(im_position[1])
+    # 切割字符
+    for i in range(im_number):
+        im_start_X = im_position[1][i][0] - xoffset
+        im_end_X = im_position[1][i][1] + xoffset
+        im_start_Y = im_position[2][i][0] - yoffset
+        im_end_Y = im_position[2][i][1] + yoffset
+        cropped = im[im_start_Y:im_end_Y, im_start_X:im_end_X]
+        cv2.imwrite(filename + '-cutting-' + str(i) + '.jpg', cropped)
+
+
+
+
+
+
+
+
+
+
+
+        # 识别验证码
+        cutting_img_num = 0
+        for file in os.listdir('./out_img'):
+            str_img = ''
+            if fnmatch(file, '%s-cutting-*.jpg' % img_name.split('.')[0]):
+                cutting_img_num += 1
+        for i in range(cutting_img_num):
+            try:
+                file = './out_img/%s-cutting-%s.jpg' % (img_name.split('.')[0], i)
+                # 识别字符
+                str_img = str_img + image_to_string(Image.open(file), lang='eng', config='-psm 10')  # 单个字符是10，一行文本是7
+            except Exception as err:
+                pass
+        print('切图：%s' % cutting_img_num)
+        print('识别为：%s' % str_img)
+
+
+def cutting_img(im, im_position, img, xoffset=1, yoffset=1):
+    filename = './out_img/' + img.split('.')[0]
+    # 识别出的字符个数
+    im_number = len(im_position[1])
+    # 切割字符
+    for i in range(im_number):
+        im_start_X = im_position[1][i][0] - xoffset
+        im_end_X = im_position[1][i][1] + xoffset
+        im_start_Y = im_position[2][i][0] - yoffset
+        im_end_Y = im_position[2][i][1] + yoffset
+        cropped = im[im_start_Y:im_end_Y, im_start_X:im_end_X]
+        cv2.imwrite(filename + '-cutting-' + str(i) + '.jpg', cropped)
