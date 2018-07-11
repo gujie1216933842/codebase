f25e4ad1d7a9bfd7d098b6cc8a52cab40f898b85
gujientsy <gujientsy@163.com>
2018-07-11 19:32:00 +0800

    aa

--- interator/02_yield生成器.py
+++ interator/02_yield生成器.py
@@ -8,3 +8,13 @@ print(next(a))
 print(next(a))
 print(next(a))
 print(next(a))
+
+'''
+函数fun中含有关键字yield,fun()是一个生成器
+a = fun()   a就是一个生成器对象
+next(a)  触发生成器,返回一个值   0,程序中断
+next(a)  触发生成器,返回一个值   1,程序中断
+
+
+
+'''
