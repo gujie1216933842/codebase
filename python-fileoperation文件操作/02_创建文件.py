c41768a40a5f75044efa2e9b5904c59e28ec504c
gujientsy <gujientsy@163.com>
2018-07-12 21:20:16 +0800

    aa

--- python-fileoperation文件操作/02_创建文件.py
+++ python-fileoperation文件操作/02_创建文件.py
@@ -5,16 +5,16 @@ import os
 # aa = input()
 # 文件名
 file_name = os.path.dirname(__file__) + '/new.txt'
-fn = open(file_name, 'w')
+fn = open(file_name, 'r+')
 
 # content = input()
 for i in range(1000):
-    fn.write(u'你大3爷\r')
+    fn.write(u'你大爷的\n')
 fn.close()
 '''
 'r'：只读（缺省。如果文件不存在，则抛出错误）
 'w'：只写（如果文件不存在，则自动创建文件）,文件常用w
-'a'：附加到文件末尾（如果文件不存在，则自动创建文件）,写.不可读
+'a'：附加到文件末尾（如果文件不存在，则自动创建文件）
 'r+'：读写（如果文件不存在，则抛出错误）
 
 '''
--- python-fileoperation文件操作/05_python逐行读取文件.py
+++ python-fileoperation文件操作/05_python逐行读取文件.py
@@ -1,11 +1,12 @@
 import os
 
 file_name = os.path.dirname(__file__) + '/new.txt'
-fn = open(file_name, 'r')  #读写
-fn1 = open(file_name, 'w')  #读写
+new_file_name = os.path.dirname(__file__) + '/newfile.txt'
+fn = open(file_name, 'r+')  #读写
+fn1 = open(new_file_name, 'w')  #读写
 
 lines = fn.readlines()
+print(lines)
 for line in lines:
-    print(line)
     fn1.write(line.replace('大','小'))
 fn.close()
