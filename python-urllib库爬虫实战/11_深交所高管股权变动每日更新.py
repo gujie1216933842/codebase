cc3f7e6de1a3a22a29bf0c653f21f6de0e2c13bb
gujie1216933842 <gujientsy@163.com>
2018-07-20 21:22:12 +0800

    aa

--- python-urllib库爬虫实战/11_深交所高管股权变动每日更新.py
+++ python-urllib库爬虫实战/11_深交所高管股权变动每日更新.py
@@ -29,27 +29,55 @@ def insert_mysql(item):
     connect.close()
 
 
+def select_mysql(sql):
+    # 连接数据库
+    connect = pymysql.Connect(
+        host='47.97.165.75',
+        port=3306,
+        user='root',
+        passwd='123',
+        db='stock',
+        charset='utf8'
+    )
+    # 获取游标
+    cursor = connect.cursor()  # 最终返回数据类型元组
+    cursor.execute(sql)  # 如果没有参数就不传,大于等于两个需要写成tuple形式
+    ret = cursor.fetchall()
+    cursor.close()
+    connect.close()
+    return ret
+
+
 if __name__ == "__main__":
-    # 当前日期
+
+    sql = "select change_date from sz_senior_stock_change_list  group  by change_date order by change_date desc"
+    ret = select_mysql(sql)
     now = datetime.date.today().strftime("%Y-%m-%d")
-    # 两天前
-    before = (datetime.datetime.now() + datetime.timedelta(days=-2)).strftime("%Y-%m-%d")
+
+    # 数据库中最新的日期
+    last_date = ret[0][0].strftime("%Y-%m-%d")
+    # 数据库中最新日期前一天
+    new_last_date = (ret[0][0] + datetime.timedelta(days=+1)).strftime("%Y-%m-%d")
+    print(new_last_date)
 
     url = "http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1801_cxda&TABKEY=tab1&PAGENO=1&txtStart=%s&txtEnd=%s&random=%s" % (
-        before, now, random.random())
+        new_last_date, now, random.random())
 
     data = urllib.request.urlopen(url).read()
     dict_data = json.loads(data)
 
     # 页数
     pages = dict_data[0]['metadata']['pagecount']
+    print(pages)
 
     for i in range(1, pages + 1, 1):
-        url = "http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1801_cxda&TABKEY=tab1&PAGENO=%s&txtStart=%s&txtEnd=%s&random=%s" % (
-            i, before, now, random.random())
+        url1 = "http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1801_cxda&TABKEY=tab1&PAGENO=%s&txtStart=%s&txtEnd=%s&random=%s" % (
+            i, new_last_date, now, random.random())
+        print(url1)
 
-        data = urllib.request.urlopen(url).read()
+        data = urllib.request.urlopen(url1).read()
         dict_data_new = json.loads(data)
-        print(dict_data_new[0]['data'])
-        for item in dict_data[0]['data']:
+
+        for item in dict_data_new[0]['data']:
+            print(item)
             insert_mysql(item)
