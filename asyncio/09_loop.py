1d8c0dbe6cb4e5f3447a7b2c236f770e5d3881fc
gujie1216933842 <gujientsy@163.com>
2018-07-29 13:48:59 +0800

    base_code

--- asyncio/09_loop.py
+++ asyncio/09_loop.py
@@ -2,13 +2,13 @@ import asyncio,time
 
 async  def  get_html(url):
     print('start get url')
-    await asyncio.sleep(2)
+    await asyncio.sleep(2)  #换成time.sleep(2) 整个程序就变成串型了
     print('end get url')
 
 
 if __name__ == "__main__":
     start_time = time.time()
     loop = asyncio.get_event_loop()
-    tasks = [get_html('http://www.baidu.com') for i in range(10)]
+    tasks = [get_html('http://www.baidu.com/{}'.format(i)) for i in range(20)]
     loop.run_until_complete(asyncio.wait(tasks))
     print('time:{}'.format(start_time-time.time()))
--- python-urllib库爬虫实战/12_请求连接返回json(aiohttp异步).py
+++ python-urllib库爬虫实战/12_请求连接返回json(aiohttp异步).py
@@ -1,24 +1,6 @@
-import urllib.request
-import json, re, random
+import json, re, random, time
 import pymysql.cursors
 import asyncio, aiohttp
-
-'''定义一个url链接池,存放待爬取的url'''
-waitting_urls = []  # 等待爬取的urls
-used_urls = set()  # 已经爬取过的urls
-
-
-def get_urls():
-    urls = []
-    for i in range(1, 3227):  # 爬取的页码随时修改
-        print(i)
-        url = "http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1801_cxda&TABKEY=tab1&PAGENO=%s&random=%s" % (
-            i, random.random())
-        if url not in waitting_urls and url not in used_urls:
-            waitting_urls.append(url)
-            urls.append(url)
-        return urls
-
 
 '''开始爬虫逻辑'''
 
@@ -35,9 +17,11 @@ async def main(url):
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     loop = asyncio.get_event_loop()
-    tasks = [main(url) for url in waitting_urls]
+    tasks = [main(
+        "http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1801_cxda&TABKEY=tab1&PAGENO=%s&random=%s" % (
+            i, random.random())) for i in range(200)]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
-
-
+    print('comsume:{}'.format(time.time() - start_time))
