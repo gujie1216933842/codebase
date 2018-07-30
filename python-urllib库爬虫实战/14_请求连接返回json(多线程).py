09e4cb680fbe8b3e9553eec6b15aeddd07255230
gujientsy <gujientsy@163.com>
2018-07-30 19:39:56 +0800

    aa

---
+++ python-urllib库爬虫实战/14_请求连接返回json(多线程).py
@@ -0,0 +1,22 @@
+from concurrent.futures import as_completed, ThreadPoolExecutor
+import time, random, requests ,json
+
+
+def get_data(url):
+    res = requests.get(url)
+    if res.status_code == 200:
+        dict_data = json.loads(res.text)
+        list_data = dict_data[0]['data']
+        print(list_data)
+        return res
+
+
+if __name__ == "__main__":
+    excutor = ThreadPoolExecutor(max_workers=100)
+    all_tasks = [excutor.submit(get_data, (
+            "http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1801_cxda&TABKEY=tab1&PAGENO=%s&random=%s" % (
+        i, random.random()))) for i in range(3000)]
+
+    '''as_complete是一个生成器'''
+    for future in as_completed(all_tasks):
+        data = future.result()
--- 多线程/15_多线程.py
+++ 多线程/15_多线程.py
@@ -3,7 +3,7 @@ import time
 
 
 def get_times(times):
-    time.sleep(times)
+    # time.sleep(times)
     print('get {} times'.format(times))
     return times
 
@@ -11,7 +11,7 @@ def get_times(times):
 excutor = ThreadPoolExecutor(max_workers=2)
 '''批量提交'''
 
-urls = [2,3,4]
+urls = [2,3,4,6,8,5]
 all_tasks = [excutor.submit(get_times, (url)) for url in urls]
 
 '''as_complete是一个生成器'''
