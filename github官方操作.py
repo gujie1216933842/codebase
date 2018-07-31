3de4330cfa2bdfa0dcb20f46c17a9112bbd87d7f
gujientsy <gujientsy@163.com>
2018-07-31 13:51:40 +0800

    aa

---
+++ github官方操作.py
@@ -0,0 +1,31 @@
+'''
+
+Quick setup — if you’ve done this kind of thing before
+or
+HTTPS
+SSH
+
+https://github.com/gujie1216933842/flask.git
+We recommend every repository include a README, LICENSE, and .gitignore.
+
+…or create a new repository on the command line
+echo "# flask" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/gujie1216933842/flask.git
+git push -u origin master
+
+
+
+…or push an existing repository from the command line
+git remote add origin https://github.com/gujie1216933842/flask.git
+git push -u origin master
+…or import code from another repository
+
+
+
+
+
+
+'''
