32b90a5825e0a3cc2dd63e133a4a019165be2f61
gujientsy <gujientsy@163.com>
2018-08-02 17:03:47 +0800

    aa

--- 多线程/20_守护线程.py
+++ 多线程/20_守护线程.py
@@ -17,9 +17,15 @@ def run2(sleep_times):
 if __name__ == '__main__':
     now = lambda: time.time()
     start_time = time.time()
+
     t1 = threading.Thread(target=run1, args=(1,))
     t2 = threading.Thread(target=run2, args=(2,))
+    '''这里面把两个线程同时设置为守护线程,主线程执行完,整个程序就停止了'''
+    t1.setDaemon(True)  # 设置守护线程,默认为false
+    t2.setDaemon(True)  # 设置守护线程,默认为false
     t1.start()
     t2.start()
+
+
     print('consume:{}s'.format(now() - start_time))
     '''主线程等待子线程执行完之后,才结束'''
